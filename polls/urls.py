25fbfdf7628541d7df57eca6463e650a6cddde59
Alyosos <lucien.bouletroblin@gmail.com>
2016-11-20 18:51:18 +0100

    Restaurant try, 9

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.conf.urls import url, include
 
+
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
--- polls/views.py
+++ polls/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'polls/post_detail.html', {'post': post})
 
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'polls/post_list.html', {'posts': posts})
 
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -28,6 +32,7 @@ def post_new(request):
     return render(request, 'polls/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
