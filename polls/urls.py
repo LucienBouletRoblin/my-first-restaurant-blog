8e4233e8dbdb7744ed335e2faf4c39d1eff08415
Alyosos <lucien.bouletroblin@gmail.com>
2016-11-20 17:03:50 +0100

    Restaurant try, 7

--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'polls/post_detail.html', {'post': post})
 
 
 def post_list(request):
